e6479f2feb8e983b90c9dc6b397c9c0cbc2e4ac9
LucaMalavolta <sWpsN7yNysYM>
2019-06-20 11:58:50 +0200

    old files removed

--- hg_commit.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo $1
-hg commit -m "$1"
-hg push ssh://hg@bitbucket.org/LMalavolta/harps_skycorr
--- hg_pull.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-hg pull ssh://hg@bitbucket.org/LMalavolta/harps_skycorr
-hg update
